f363d566da09b33d0d48e945bbabb0ffd71f8f73
Shadhin <113816422+shadhin-music@users.noreply.github.com>
2026-06-08 16:26:10 +0600

    Release 1.0.8 — update binary target URL and checksum

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MYBLShadhinSDK",
-            url: "https://github.com/shadhin-music/MYBLMusicSPM-iOS/releases/download/1.0.2/MYBLShadhinSDK.xcframework.zip",
-            checksum: "6dc5f5c569b2d0019568ed52177d15be3c81788f98e76402326708b95c10dca2"
+            url: "https://github.com/shadhin-music/MYBLMusicSPM-iOS/releases/download/1.0.8/MYBLShadhinSDK.xcframework.zip",
+            checksum: "101b361cc138474a9a6a7259de92790aa60e8091279c68c3a91363141db3c91c"
         )
     ]
 )
